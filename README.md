b5cce23e091d852d3f8f6e3fa3df0c351dbad9bf
Wouter Lievens <lievenswouter@gmail.com>
2016-11-17 16:59:46 +0100

    initial checkin

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# expedite
-Javascript Expression Editor Environment
+# Expedite
+
+**Expedite** is a javascript **Exp**ression **Edit**or **E**nvironment. It's a simple jQuery plugin for authoring infix expressions in an ergonomic fashion. These expressions can then feature in search filters, queries, rule engines, or anything else you fancy that needs an expression system.
+
+**Expedite** itself does not implement an expression evaluator. It's there only to let your user compose arithmetic and logical expressions without having to type code.
---
+++ expedite.js
@@ -0,0 +1,127 @@
+;(function ($, window, document, undefined) {
+
+    'use strict';
+
+    // undefined is used here as the undefined global variable in ECMAScript 3 is
+    // mutable (ie. it can be changed by someone else). undefined isn't really being
+    // passed in so we can ensure the value of it is truly undefined. In ES5, undefined
+    // can no longer be modified.
+
+    // window and document are passed through as local variables rather than global
+    // as this (slightly) quickens the resolution process and can be more efficiently
+    // minified (especially when both are regularly referenced in your plugin).
+
+    // Create the defaults once
+    var pluginName = 'expedite';
+    var defaults = {
+        parenScale: 25,
+        expression: []
+    };
+
+    // The actual plugin constructor
+    function Plugin(element, options) {
+        this.element = element;
+
+        // jQuery has an extend method which merges the contents of two or
+        // more objects, storing the result in the first object. The first object
+        // is generally empty as we don't want to alter the default options for
+        // future instances of the plugin
+        this.settings = $.extend({}, defaults, options);
+        this._defaults = defaults;
+        this._name = pluginName;
+        this.init();
+    }
+
+    // Avoid Plugin.prototype conflicts
+    $.extend(Plugin.prototype, {
+        init: function () {
+            var maxDepth = this.getMaxDepth(this.settings.expression);
+            var $tree = this.populate(this.settings.expression, 0, maxDepth);
+            $(this.element).addClass('expedite');
+            $(this.element).append($tree);
+        },
+        getMaxDepth: function (expression) {
+            var depth = 0;
+            for (var n = 1; n < expression.length; ++n) {
+                depth = Math.max(depth, this.getMaxDepth(expression[n]) + 1);
+            }
+            return depth;
+        },
+        populate: function (expression, depth, maxDepth) {
+            var $span = $('<span>');
+
+            if (expression.length == 3) {
+                // Binary infix
+
+                var parenSize = (100 + (maxDepth - depth) * this.settings.parenScale) + '%';
+
+                var $paren1 = $('<span>(</span>');
+                $paren1.addClass('expedite-paren');
+                $paren1.css('font-size', parenSize);
+                $span.append($paren1);
+
+                var $operator = $('<span>');
+                $operator.addClass('expedite-operator');
+                $operator.text(expression[0]);
+
+                var $inner = $('<span>');
+                $inner.append(this.populate(expression[1], depth + 1, maxDepth));
+                $inner.append($operator);
+                $inner.append(this.populate(expression[2], depth + 1, maxDepth));
+                $span.append($inner);
+
+                var $paren2 = $('<span>)</span>');
+                $paren2.addClass('expedite-paren');
+                $paren2.css('font-size', parenSize);
+                $span.append($paren2);
+
+                var hoverIn = function () {
+                    $paren1.addClass('expedite-hover');
+                    $paren2.addClass('expedite-hover');
+                    $inner.addClass('expedite-inner-hover');
+                };
+
+                var hoverOut = function () {
+                    $paren1.removeClass('expedite-hover');
+                    $paren2.removeClass('expedite-hover');
+                    $inner.removeClass('expedite-inner-hover');
+                };
+
+                $paren1.hover(hoverIn, hoverOut);
+                $paren2.hover(hoverIn, hoverOut);
+            } else if (expression.length == 2) {
+                // Unary prefix
+
+                var $operator = $('<span>');
+                $operator.addClass('expedite-operator');
+                $operator.text(expression[0]);
+
+                $span.append($operator);
+                $span.append(this.populate(expression[1], depth + 1, maxDepth));
+            } else if (typeof expression === 'number') {
+                // Nullary number
+
+                $span.addClass('expedite-number');
+                $span.append(expression);
+            } else {
+                // Nullary variable
+
+                $span.addClass('expedite-variable');
+                $span.append(expression);
+            }
+            return $span;
+        }
+    });
+
+// A really lightweight plugin wrapper around the constructor,
+// preventing against multiple instantiations
+    $.fn[pluginName] = function (options) {
+        return this.each(function () {
+            if (!$.data(this, 'plugin_' + pluginName)) {
+                $.data(this, 'plugin_' +
+                    pluginName, new Plugin(this, options));
+            }
+        });
+    };
+})
+(jQuery, window, document);
